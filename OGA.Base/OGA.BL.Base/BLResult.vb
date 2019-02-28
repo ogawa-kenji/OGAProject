93f15a986f0a895c95988f74730c63ca4dba4f61
ogawa-kenji <ogawa.kenji.mail@gmail.com>
2019-02-28 21:20:15 +0900

    commit

---
+++ OGA.Base/OGA.BL.Base/BLResult.vb
@@ -0,0 +1,11 @@
+﻿Public Class BLResult
+    Public Enum ReturnCds As Integer
+        _NORMAL
+        _WARNING
+        _ERROR
+    End Enum
+
+    Public Property ReturnCd As ReturnCds
+    Public Property Message As String
+
+End Class
